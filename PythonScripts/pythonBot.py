6820a784929e7e6701293cb3bd730b5ab5014ab2
Aristide Thomas Mendoo <mendoo.aristide@gmail.com>
2011-07-17 13:26:52 +0200

    bot en python

---
+++ PythonScripts/pythonBot.py
@@ -0,0 +1,41 @@
+import urllib
+import simplejson as json
+
+class TranslateFailed(Exception):
+    pass
+
+class UNknownWxception(Exception):
+    pass
+    
+class AristideBot(object):
+
+    URI="http://ajax.googleapis.com/ajax/services/language/translate"
+    default={'v':'1.0'}
+    
+    def __init(self):
+        return
+
+    def build_request(self,args):
+        args.update({
+            'langpair':'%s%%7C%s'%(self.src,self.to),
+            'q':urllib.quote_plus(self.phrase),
+        })
+        return '%s' %('&'.join(['%s=%s' %(k,v) for (k,v) in args.iteritems()])) 
+
+    def retrieveAnswer(self,response):
+        if response['responseStatus'] == 200:
+            return response['responseData']['translatedText']
+        else:            
+            return self.phrase
+            
+
+    def translate(self,src,to,phrase):
+        self.src,self.to,self.phrase = src,to,phrase
+        args=self.default.copy()
+        return self.retrieveAnswer(json.load(urllib.FancyURLopener().open('%s?%s' % (self.URI,self.build_request(args)))))
+
+######## Usage 
+
+translator=AristideBot()
+text="bonjour Aristide"
+print translator.translate('fr','enbb',text)
---
+++ README.txt
@@ -0,0 +1,13 @@
+Hi  !!!! 
+
+These are scripts I'm writing just for fun
+to spend my spare time
+
+fill free to bring critis and add-ons
+
+email me on "mendoo.aristide@gmail.com"
+
+thank's  
+
+
+
